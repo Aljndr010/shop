7f15a2f2f3191d469683d76007ab503debe04f0c
Aljndr010 <145481325+Aljndr010@users.noreply.github.com>
2023-12-10 19:42:53 +0100

    implementación script de seguimiento de matomo

--- public/productos.php
+++ public/productos.php
@@ -30,6 +30,21 @@
             margin-top: 10px;
         }
     </style>
+    <!-- Matomo -->
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//localhost/matomo/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
 </head>
 
 <body>
@@ -60,6 +75,13 @@
             </ul>
         </div>
     </nav>
+
+        <!-- Sección del Producto Destacado -->
+        <div class="container mt-4 featured-product">
+        <h2>Producto Destacado</h2>
+        <img src="ruta/a/imagen_del_producto_destacado.jpg" alt="Producto Destacado">
+        <p class="featured-product-description">Descripción atractiva del producto destacado. Puedes resaltar las características especiales y promociones aquí.</p>
+    </div>
 
     <div class="container mt-4">
         <div class="row">
